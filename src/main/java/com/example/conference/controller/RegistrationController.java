ab755d601c5a13ccf866a14e662e6a77434692aa
Nishant Bodade <nishantbodade@gmail.com>
2022-02-28 10:00:15 +0530

    post mapping added form added Registartion Model added @ModelAttribute is added and tight up with our model and view

--- src/main/java/com/example/conference/controller/RegistrationController.java
+++ src/main/java/com/example/conference/controller/RegistrationController.java
@@ -1,7 +1,10 @@
 package com.example.conference.controller;
 
+import com.example.conference.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -9,9 +12,13 @@ import java.util.Map;
 public class RegistrationController {
 
     @GetMapping("registration")
-    public String getRegistration(Map<String,Object> model){
-        
+    public String getRegistration(@ModelAttribute("registration")Registration registration) {
         return "registration";
+    }
 
+    @PostMapping("registration")
+    public String addRegistration(@ModelAttribute ("registration") Registration registration) {
+        System.out.println("Registration: " + registration.getName());
+        return "registration";
     }
 }
